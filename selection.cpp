76a99ace106d9d02786dde7f73fb3843ca35b12a
Yasin Sensoy <73845925+sensoyyasin@users.noreply.github.com>
2025-11-10 22:41:56 -0800

    Update selection.cpp

--- selection.cpp
+++ selection.cpp
@@ -1,19 +1,1 @@
-#include "main.hpp"
 
-void selectionSort(vector<int> &arr, int size)
-{
-	int i = 0;
-	while (i < size)
-	{
-		int j = i + 1;
-		int bestMinIndex = i;
-		while (j < size)
-		{
-			if (arr[j] < arr[bestMinIndex])
-				bestMinIndex = j;
-			j += 1;
-		}
-		swap(arr[i], arr[bestMinIndex]); // find min index and put zero.
-		i += 1;
-	}
-}
